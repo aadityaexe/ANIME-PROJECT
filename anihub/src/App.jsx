cbb935bfb7cbebc6ec524e2874e7c887b66570c3
Aditya Kashyap <adityakashyap7998@gmail.com>
2024-08-13 22:50:35 +0530

    extensions and nav added

--- anihub/src/App.jsx
+++ anihub/src/App.jsx
@@ -1,9 +1,10 @@
-import "./App.css";
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
-      <h1>hi ksyp</h1>
+            <Navbar />
+            
     </>
   );
 }

bc2f80d1d5ca98287e129a8a0a38bd9ed825c912
adityaexe <kmradi2002@gmail.com>
2024-08-14 22:53:51 +0530

    op of title

--- anihub/src/components/Title/Title.jsx
+++ anihub/src/components/Title/Title.jsx
@@ -2,7 +2,7 @@
 
 const Title = ({ title, subTitle }) => {
   return (
-    <section className=" py-[30px] dark:bg-dark bg-opacity-20 bg-yellow-300">
+    <section className=" py-[30px] dark:bg-dark bg-opacity-10 bg-yellow-300">
       <div className="mx-auto px-4 sm:container">
         <div className="border-l-[5px] border-primary pl-5">
           <h2 className="mb-2 text-2xl font-semibold text-dark dark:text-white">
